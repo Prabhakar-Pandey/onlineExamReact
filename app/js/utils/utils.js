b85c9e8aea28254eacf221151d3675306117d0c8
prabhakar <prabhakar.pandey@games24x7.com>
2017-08-16 20:42:11 +0530

    xhr call parameter updated

--- app/js/utils/utils.js
+++ app/js/utils/utils.js
@@ -1,7 +1,6 @@
 import Data from '../data/data';
 var Utils = {
     getRequest:function(url,callback){
-        url = "https://cdn.rawgit.com/santosh-suresh/39e58e451d724574f3cb/raw/784d83b460d6c0150e338c34713f3a1c2371e20a/assignment.json";
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             //console.log('hi',this.readyState,this.status,this)
