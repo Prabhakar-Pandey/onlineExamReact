ce5a7d4c6463e357c0b1c7d1e0f0fbc23658293a
prabhakar <prabhakar.pandey@games24x7.com>
2017-08-16 20:31:01 +0530

    checking

--- app/js/utils/utils.js
+++ app/js/utils/utils.js
@@ -4,10 +4,10 @@ var Utils = {
         url = "https://cdn.rawgit.com/santosh-suresh/39e58e451d724574f3cb/raw/784d83b460d6c0150e338c34713f3a1c2371e20a/assignment.json";
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
-            console.log('hi',this.readyState,this.status,this)
+            //console.log('hi',this.readyState,this.status,this)
             if (this.readyState == 4 && this.status == 200) {
                 console.log(this.responseText,"response obj");
-                return callback("true")
+                //return callback("true")
             }
         };
         xhttp.open("GET", url, true);
